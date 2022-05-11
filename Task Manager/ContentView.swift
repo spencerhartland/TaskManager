0a16d7a85bb39e860189615c5f05f33fec9cc62f
Spencer Hartland <shartland@apple.com>
2022-05-10 19:38:49 -0700

    Initial Commit

---
+++ Task Manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Task Manager
+//
+//  Created by Spencer Hartland on 5/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task Manager/Task_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_ManagerApp.swift
+//  Task Manager
+//
+//  Created by Spencer Hartland on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Task_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
